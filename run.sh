175d971f57a2ec2482f03f805b386c8d29b577ef
rhoposit <j.williams@ed.ac.uk>
2021-03-20 19:15:00 +0000

    update

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ DATA=/home/smg/v-j-williams/workspace/external_modified/data/sys5_txt_mixed
 CHECKPOINTS=/home/smg/v-j-williams/workspace/external_modified/sys5_checkpoints
 HPARAM=hparams_modified.py
 
-train.py --dataset=$DATASET --data-root=$DATA --checkpoint-dir=$CHECKPOINTS --hparams=$HPARAM
+python train.py --dataset=$DATASET --data-root=$DATA --checkpoint-dir=$CHECKPOINTS --hparams=$HPARAM
 
 
 # INFERENCE command to run
-#synthesize.py  --dataset=$DATASET --data-root=$DATA --checkpoint-dir=$CHECKPOINTS --postnet-checkpoint-dir=</path/to/postnet/model/dir> --hparams=$HPARAM
+#python synthesize.py  --dataset=$DATASET --data-root=$DATA --checkpoint-dir=$CHECKPOINTS --postnet-checkpoint-dir=</path/to/postnet/model/dir> --hparams=$HPARAM
--- tacotron/losses.py
+++ tacotron/losses.py
@@ -27,7 +27,22 @@ def mse_loss(y_hat, y, mask):
         return loss
 
 
+def codes_lossBAD(y_hat, y, mask, codes_loss_type):
+    cce = tf.keras.losses.CategoricalCrossentropy()
+    y = tf.Print(y, [tf.shape(y)], "\n* y before reshape\n")
+    y_hat = tf.Print(y_hat, [tf.shape(y_hat), tf.shape(y_hat)[0], tf.shape(y_hat)[1], tf.shape(y_hat)[2]], "\n* yhat before reshape\n")
+    batch = tf.shape(y_hat)[0]
+    length = tf.shape(y_hat)[1]
+    width = tf.shape(y_hat)[2]
+    y =tf.reshape(y, tf.shape(y_hat))
+    y = tf.Print(y, [tf.shape(y)], "\n* y after reshape\n")
+    return cce(y, y_hat)
+
+
+
 def codes_loss(y_hat, y, mask, codes_loss_type):
+#    y = tf.Print(y, [tf.shape(y)], "\n* y shape\n")
+#    y_hat = tf.Print(y_hat, [tf.shape(y_hat)], "\n* y_hat shape\n")
     return tf.losses.softmax_cross_entropy(y, y_hat, weights=mask)
 
 
@@ -36,7 +51,8 @@ def classification_loss(y_hat, y, mask):
 
 
 def binary_loss(done_hat, done, mask):
-#    done = tf.Print(done, [tf.shape(done)], "done")
-#    done_hat = tf.Print(done_hat, [tf.shape(done_hat)], "done_hat")
-#    mask = tf.Print(mask, [tf.shape(mask)], "mask")   
-    return tf.losses.sigmoid_cross_entropy(done, tf.squeeze(done_hat, axis=-1), weights=mask)
+    done_hat = tf.squeeze(done_hat, axis=-1)
+    done = tf.Print(done, [tf.shape(done), done], "done", summarize=-1)
+    done_hat = tf.Print(done_hat, [tf.shape(done_hat), done_hat], "done_hat", summarize=01)
+    mask = tf.Print(mask, [tf.shape(mask), mask], "mask")   
+    return tf.losses.sigmoid_cross_entropy(done, done_hat, weights=mask)
